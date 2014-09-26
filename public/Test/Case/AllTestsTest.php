4fb75d7944dc4c384f053ed3045dd8e0683c82f7
root <root@localhost.localdomain>
2014-09-26 09:09:22 +0000

    Test Jenkins 2

---
+++ public/Test/Case/AllTestsTest.php
@@ -0,0 +1,10 @@
+<?php 
+class AllTestsTest extends CakeTestSuite {
+	public static function suite(){
+		$suite = new CakeTestSuite('All Tests');
+		$suite->addTestDirectoryRecursive(TESTS,'Case');
+		return $suite;
+	}
+}
+
+?>
